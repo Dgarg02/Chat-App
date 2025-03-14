0818133d453a51b39b536167ecbbb722217e5493
Dgarg02 <dgarg061202@gmail.com>
2025-03-14 12:07:33 +0000

    backend

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import cloudinary from "../lib/cloudinary.js";
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs"
@@ -90,5 +91,28 @@ export const logout = async(req,res)=>{
     }
 }
 export const updateProfile=async(req,res)=>{
+    
+    try {
+        const{profilePic}= req.body;
+        const userId= req.user._id;
 
+        if(!profilePic){
+            return res.status(404).json({message:"Profile Pic is required"})
+        }
+        const uploadResponse = await cloudinary.uploader.upload(profilePic)
+        const updateUser= await User.findByIdAndUpdate(userId,{profilePic:uploadResponse.secure_url},{new:true})
+        res.status(200).json(updateUser)
+
+    } catch (error) {
+        console.log("Error in Updateprofile",error.message)
+        return res.status(501).json({message:"internal server error"})
+    }
+}
+export const checkAuth = async(req,res)=>{
+    try {
+        res.status(200).json(req.user);
+    } catch (error) {
+        console.log("error in checkAuth controller", error.message)
+        res.status(500).json({message:"Internal server error"})
+    }
 }
